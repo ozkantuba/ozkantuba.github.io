2674c6bb903e0d80fefe1b93526f1f5609b0e52f
Tuba Ozkan <ozkat506@newschool.edu>
2021-01-31 23:14:28 -0500

    update people
    update people

--- marinarchitects/script2.js
+++ marinarchitects/script2.js
@@ -1,65 +1,319 @@
+var peopleCard = [
 
-		var clicks;
+    {
+    	id: "andreaacunad",
+        Name: "Andrea Acuna",
+        Group: "Orange",
+        Email: "aacuna@marinarchitects.com",
+        Extention: "126",
+        photo_url: "./img/andrea.jpg"
+
+    },
+
+    {
+    	id: "astonsydneyd",
+        Name: "Aston Sydney",
+        Group: "Red",
+        Email: "asydney@marinarchitects.com",
+        Extention: "115",
+        photo_url: "./img/aston.jpg"
+    },
+
+    {
+    	id: "brookemcgowand",
+        Name: "Brooke McGowan",
+        Group: "Marketing",
+        Email: "bmcgowan@marinarchitects.com",
+        Extention: "119 / 148",
+        photo_url: "./img/brooke.jpg"
+    },
+
+    {
+
+		id: "christinamohand",
+        Name: "Christina Mohan ",
+        Group: "None",
+        Email: "cmohan@marinarchitects.com",
+        Extention: "106 / 154",
+        photo_url: "./img/empty.jpg"
+    },
+
+    {
+    	id: "dennymorand",
+        Name: "Denny Moran",
+        Group: "Blue",
+        Email: "dmoran@marinarchitects.com",
+        Extention: "117",
+        photo_url: "./img/denny.jpg"
+    },
+
+    {
+    	id: "dongshind",
+        Name: "Dong Shin",
+        Group: "Yellow",
+        Email: "dshin@marinarchitects.com",
+        Extention: "122",
+        photo_url: "./img/dong.jpg"
+    },
+
+    {
+    	id: "elifunsald",
+        Name: "Elif Unsal",
+        Group: "Yellow",
+        Email: "eunsal@marinarchitects.com",
+        Extention: "123 / 145",
+        photo_url: "./img/elif.jpg"
+    },
+
+    {
+    	id: "emreozdemird",
+        Name: "Emre Ozdemir",
+        Group: "Red",
+        Email: "eozdemir@marinarchitects.com",
+        Extention: "147",
+        photo_url: "./img/emre.jpeg"
+    },
+
+    {
+    	id: "enmanuelespinald",
+        Name: "Enmanuel Espinal",
+        Group: "Orange",
+        Email: "eespinal@marinarchitects.com",
+        Extention: "118",
+        photo_url: "./img/empty.jpg"
+    },
+
+        {
+        id: "faithmasid",
+        Name: "Faith Masi",
+        Group: "Marketing",
+        Email: "fmasi@marinarchitects.com",
+        Extention: "unknown",
+        photo_url: "./img/empty.jpg",
+    },
+
+    {
+    	id: "fatihergund",
+        Name: "Fatih Ergun",
+        Group: "Red",
+        Email: "fergun@marinarchitects.com",
+        Extention: "133 / 146",
+        photo_url: "./img/fatih.jpg",
+    },
+
+    {
+    	id: "germanramirezd",
+        Name: "German Ramirez",
+        Group: "Orange",
+        Email: "gramirez@marinarchitects.com",
+        Extention: "114 / 111",
+        photo_url: "./img/german.jpg"
+
+    },
+
+    {
+    	id: "juanfawcettd",
+        Name: "Juan Fawcett",
+        Group: "Red",
+        Email: "jfawcett@marinarchitects.com",
+        Extention: "130",
+        photo_url: "./img/juan.jpg"
+    },
+
+    {
+    	id: "keremmarind",
+        Name: "Kerem Marin",
+        Group: "Blue",
+        Email: "kmarin@marinarchitects.com ",
+        Extention: "152",
+        photo_url: "./img/kerem.jpg"
+    },
+
+    {
+    	id: "kathleenriopeld",
+        Name: "Kathleen Riopel",
+        Group: "Purple",
+        Email: "kriopel@marinarchitects.com",
+        Extention: "120",
+        photo_url: "./img/kathleen.jpg"
+    },
+
+    {
+    	id: "laurencarbonarod",
+        Name: "Lauren Carbonaro",
+        Group: "Blue",
+        Email: "lcarbonaro@marinarchitects.com",
+        Extention: "135",
+        photo_url: "./img/lauren.jpg"
+    },
+
+    {
+    	id: "leodiazmontield",
+        Name: "Leo Diaz-Montiel",
+        Group: "Yellow",
+        Email: "ldiaz@marinArchitects.com",
+        Extention: "110",
+        photo_url: "./img/leo.jpg"
+    },
+
+    {
+    	id: "leydinunezd",
+        Name: "Leydi Nunez",
+        Group: "Blue",
+        Email: "lnunez@marinArchitects.com",
+        Extention: "144",
+        photo_url: "./img/leydi.jpg"
+    },
+
+    {
+    	id: "luismejiad",
+        Name: "Luis Mejia ",
+        Group: "Orange",
+        Email: "lmejia@marinarchitects.com",
+        Extention: "129",
+        photo_url: "./img/luis.jpg"
+    },
+
+    {
+    	id: "martharomerod",
+        Name: "Martha Romero",
+        Group: "Green",
+        Email: "mromero@marinarchitects.com",
+        Extention: "143 /134",
+        photo_url: "./img/martha.jpg"
+    },
+
+    {
+    	id: "mingchuangd",
+        Name: "Ming Chuang ",
+        Group: "Red",
+        Email: "mchuang@marinarchitects.com",
+        Extention: "112",
+        photo_url: "./img/ming.jpg"
+    },
+
+    {
+    	id: "minkukkimd",
+        Name: "Minkuk Kim",
+        Group: "Yellow",
+        Email: "mkim@marinarchitects.com",
+        Extention: "109",
+        photo_url: "./img/minkuk.jpg"
+    },
+
+    {
+    	id: "mohammedalamind",
+        Name: "Mohammed Alamin",
+        Group: "Red",
+        Email: "malamin@marinarchitects.com",
+        Extention: "134",
+        photo_url: "./img/mohammed.jpg"
+    },
+
+    {
+    	id: "nekahighlandd",
+        Name: "Neka Highland",
+        Group: "Blue",
+        Email: "nhighland@marinarchitects.com",
+        Extention: "105",
+        photo_url: "./img/neka.jpg"
+    },
+
+    {
+    	id: "shellymohand",
+        Name: "Shelly Mohan",
+        Group: "Green",
+        Email: "smohan@marinarchitects.com",
+        Extention: "128",
+        photo_url: "./img/shelly.jpg"
+    },
+
+
+    {
+        id: "sherlypoluscard",
+        Name: "Sherly Poluscar",
+        Group: "Blue",
+        Email: "spoluscar@marinarchitects.com",
+        Extention: "unknown",
+        photo_url: "./img/empty.jpg"
+    },
+
+    {
+    	id: "waltermarind",
+        Name: "Walter Marin",
+        Group: "Black",
+        Email: "wmarin@marinArchitects.com",
+        Extention: "108",
+        photo_url: "./img/walter.jpg"
+    },
+
+    {
+    	id: "conferenceroomsmall",
+        Name: "Conference Room S",
+        Group: "none",
+        Email: "none",
+        Extention: "101",
+        photo_url: "./img/empty.jpg"
+
+    },
+
+    {
+    	id: "conferenceroommedium",
+        Name: "Conference Room M",
+        Group: "none",
+        Email: "none",
+        Extention: "102",
+        photo_url: "./img/empty.jpg"
+
+    },
+
+    {
+        id: "conferenceroomlarge",
+        Name: "Conference Room L",
+        Group: "none",
+        Email: "none",
+        Extention: "100",
+        photo_url: "./img/empty.jpg"
+    }
+
+]
+
+var clicks;
+var hovers;
 
 		$(document).ready(function(){
 
-		$( "input" ).each(function() {
-			var name = this.id;
-			var distance = ("#" + name + "c")
-			$(distance).css({"display": "none"});
 
-		});
+//  for hovering desks 
+            //     $(".peoplelist").each(function() {
 
-		$( "input" ).each(function() {
+            //     $(this).mouseover(function(){
 
-			$( this ).click(function(){
+            //     var name = this.id;
+            //     var personName = '#' + name;
 
-				   var name = this.id;
-				   console.log(name);
-		  		   var id = "#" + name;
-		           var label = ("label[for=" + name +"]");
-		  		   var distance = ("#" + name + "c");
-		    	   clicks = $(this).data('clicks');
+            //    $(personName).css({"stroke": "#9CC2F2", "stroke-opacity": "0.8", "stroke-width":"8", "fill-opacity": "1"});
 
-		    	if(clicks){
+            //     })
+            //     .mouseout(function() {
 
-		    		 console.log("hide" + distance);
-		    		 $(distance).css({"display": "none"});
-		         $(label).css({"color": "#000"});
-		    	
-		    	} else{
+            //     var name = this.id;
+            //     var personName = '#' + name;
 
-		    		 console.log("show" + distance);
-		    		 $(distance).css({"display": "block"});
-		         $(label).css({"color": "#2196F3"});
+            //     if (clicks){
 
-		    	}
+            //        $(personName).css({"stroke": "#9CC2F2", "stroke-opacity": "0.8", "stroke-width":"16", "fill-opacity": "1"});
 
-		    $(this).data("clicks", !clicks);
-		      
-		    });
+            //     }else{
 
-		});
+            //        $ (personName).css({"stroke": "#408CDD", "stroke-width":"0.5", "fill-opacity": "0.8"});
+
+            //     }
 
 
-			$ ("button").click(function(){
+            //     });
 
-				$('input[type="checkbox"]:checked').prop('checked',false);
-		    	
-		    	$ ("input").removeData();
-		    	console.log("map is cleared");
-
-				$( "input" ).each(function() {
-
-					   var name = this.id;
-		         var label = ("label[for=" + name +"]");
-		  			 var distance = ("#" + name + "c");
-		    		 $(distance).css({"display": "none"});
-		         $(label).css({"color": "#000"});
-
-				});
-
-			});
+            // });
 
 // for clicking desks
 			$(".peoplelist").each(function() {
@@ -71,27 +325,90 @@
 			    console.log("this clicked");
 			    var name = this.id;
 			    var personName = '#' + name;
-			    var personCard = '#' + name + 'card';
+                
+                var cardName;
+                var cardTeam;
+                var cardEmail;
+                var cardExtention;
+                var cardImg;
+			   
+			    console.log(name);
 
-			    $(personCard).css({"display": "none"});
 			    clicks = $(this).data('clicks');
 
 				if(clicks){
 
+					// return the styling of visual indicator back to default 
 					$(personName).css({"stroke": "#2E68B0", "stroke-width":"0.5", "fill-opacity": "0.8"});
-					$(personCard).css({"display": "none"});
-				
+				    
+                    cardName = "Click on a desk to review.";
+                    cardTeam = "";
+                    cardEmail = "";
+                    cardExtention = "";
+                    cardTeam = "";
+                    cardImg = "./img/empty.jpg";
+
+                    $("#name").html(cardName);
+                    $("#email").html(cardEmail);
+                    $("#extention").html(cardExtention);
+                    $("#team").html(cardTeam);
+                    $("#img").attr("src",cardImg);    
+
 				} else {
 
-					$(personName).css({"stroke": "#9CC2F2", "stroke-opacity": "0.8", "stroke-width":"16", "fill-opacity": "1"});
-					$(personCard).css({"display": "block"});
-					console.log(personCard);
+					// change the styling of visual indicator 
+					$(personName).css({"stroke": "#9CC2F2", "stroke-opacity": "0.8", "stroke-width":"12", "fill-opacity": "1"});
+					
+					// change the card info for each person - pull data from the peopleCard array of objects 
+					// missing objects for empty desks 
+					const items = peopleCard.filter(item => item.id.indexOf(name) !== -1);
+			
+		
+					if(items[0]==null){
+					
+						cardName = "This desk has not assigned yet.";
+						cardTeam = "";
+						cardEmail = "";
+						cardExtention = "";
+                        cardTeam = "";
+						cardImg = "./img/empty.jpg";
+
+						$("#name").html(cardName);
+						$("#email").html(cardEmail);
+						$("#extention").html(cardExtention);
+                        $("#team").html(cardTeam);
+						$("#img").attr("src",cardImg);
+
+					} else {
+
+                        $("#cardDefault").html("");
+						
+                        cardName = items[0].Name;
+						cardTeam = items[0].Group;
+						cardEmail = items[0].Email;
+						cardExtention = items[0].Extention;
+						cardImg = items[0].photo_url;
+
+						$("#name").html(cardName);
+						$("#email").html("<strong>e-mail</strong><br>" + cardEmail);
+						$("#extention").html("<strong>Extention</strong><br>" + cardExtention);
+                        $("#team").html("<strong>Team</strong><br>" + cardTeam);
+						$("#img").attr("src",cardImg);
+
+					}
+
 				}
 
 				$(this).data("clicks", !clicks);
 
 			    });
-				
-		  });
 
-		});
+            });
+
+
+
+
+				
+});
+
+
